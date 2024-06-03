b85eacf9abff21773b76e45089cbecd5cb28f3aa
jinny-l <jinny.l.dev@gmail.com>
2024-06-03 23:35:02 +0900

    chore: `api:ggv` 모듈 생성

---
+++ app/api/ggv/src/test/java/GgvApplicationTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
